a3fc63fec55c22b961a9cbb7dbc20541ef4df964
Sensei <bulgakovMO@gmail.com>
2022-06-20 21:54:49 +0300

    дз  пhw js server menu from yana

--- first_server/server.js
+++ first_server/server.js
@@ -9,11 +9,19 @@ var app = express();
 app.get('/index', function(req, res) {
     res.sendfile('index.html');
 });
-app.get('/', function(req, res) {
+app.get('/hi', function(req, res) {
     res.sendfile('hi.html');
 });
-
+app.get('/fine', function(req, res) {
+    res.sendfile('fine.html');
+});
+app.get('/you', function(req, res) {
+    res.sendfile('you.html');
+});
+app.get('/too', function(req, res) {
+    res.sendfile('too.html');
+});
 // запускаем сервер на порту 8080
-app.listen(3000);
+app.listen(8080);
 // отправляем сообщение
 console.log('Сервер стартовал!');
